81f017d72e65545d20cd8356eb7c49370f591bc4
antonwessel <wessel.anton@outlook.com>
2024-10-02 16:06:02 +0200

    add timer and new game

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 let homeScore = 0
 let guestScore = 0
-let homeScoreEl = document.getElementById("home-score-el")
-let guestScoreEl = document.getElementById("guest-score-el")
+
+let homeScoreEl = document.getElementById("home-score")
+let guestScoreEl = document.getElementById("guest-score")
+
+let timer
+let seconds = 0
+let minutes = 0
+
+window.onload = function () {
+    startTimer()
+}
 
 function incrementHome(amount) {
     homeScore += amount
-    homeScoreEl.textContent = homeScore
+    updateScores()
 }
 
 function incrementGuest(amount) {
     guestScore += amount
+    updateScores()
+}
+
+function newGame() {
+    homeScore = 0
+    guestScore = 0
+    updateScores()
+    resetTimer()
+    startTimer()
+}
+
+function updateScores() {
+    homeScoreEl.textContent = homeScore
     guestScoreEl.textContent = guestScore
+}
+
+function startTimer() {
+    if (!timer) {
+        timer = setInterval(function () {
+            seconds++
+            if (seconds === 60) {
+                seconds = 0
+                minutes++
+            }
+            document.getElementById("timer").textContent = minutes + ":" + (seconds < 10 ? "0" + seconds : seconds)
+        }, 1000)
+    }
+}
+
+function resetTimer() {
+    clearInterval(timer)
+    timer = null
+    seconds = 0
+    minutes = 0
+    document.getElementById("timer").textContent = "0:00"
 }
